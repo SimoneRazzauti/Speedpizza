0e0e97aa366729b3815a12ba67bb3718d1b80f6e
SimoneRazzauti <s.razzauti97@gmail.com>
2023-07-11 20:18:31 +0200

    update

--- 549451_Razzauti/index.php
+++ 549451_Razzauti/index.php
@@ -1,52 +1,48 @@
 <?php
   session_start();
+  include("checkCOOKIE.php"); //se l'utente è Loggato non richiede i dati ma utilizza i cookie salvati
 ?>
 
 <!DOCTYPE html>
 <html lang="it">
-<meta charset="UTF-8">
+  <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="css/footer.css">
-
-<head>
-	<title> FantaBall HOME </title>
-
-	<link rel="stylesheet" href="css/styleIndex.css"  type="text/css">
-	<link rel="stylesheet" href="css/stileMenu.css"  type="text/css">
-
-</head>
-<body>
-<?php
-include("checkCOOKIE.php"); //se l'utente è Loggato non richiede i dati ma utilizza i cookie salvati
-?>
-
-<?=template_menu()?>
-
-
-
-
-  <div class="container">
-  <div class="slider">
-    <img class="active" src="./immagini/home1.jpg">
-    <img src="./immagini/home2.jpg">
-    <img src="./immagini/home3.jpg">
-  </div>
-  <nav class="slider-nav">
-    <ul>
-      <li class="arrow">
-        <button class="previous">
-          PREV
-        </button>
-      </li>
-      <li class="arrow">
-        <button class="next">
-          NEXT
-        </button>
-      </li>
-    </ul>
-  </nav>
-</div>
+  <link rel="stylesheet" href="css/footer.css">
+  
+  <head>
+    <title> FantaBall HOME </title>
+    <link rel="icon" href = "./immagini/favicon.ico" type = "image/x-icon">
+    <link rel="stylesheet" href="css/styleIndex.css"  type="text/css">
+    <link rel="stylesheet" href="css/stileMenu.css"  type="text/css">
+  </head>
+  
+  <body>
+    <div class="container">
+      <div class="slider">
+        <img class="active" src="./immagini/homepage.png">
+        <img src="./immagini/homepage.png">
+        <img src="./immagini/homepage.png">
+      </div>
+      <nav class="slider-nav">
+        <ul>
+          <li class="arrow">
+            <button class="previous">
+              PREV
+              <!-- <img src="./immagini/LeftArrow.png" alt="leftArrow">
+              -->
+            </button>
+          </li>
+          <li class="arrow">
+            <button class="next">
+              <!-- <img src="./immagini/RightArrow.png" alt="rightArrow">
+              -->
+              NEXT
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
 
 <script>
 	const items = document.querySelectorAll('img');
@@ -96,6 +92,7 @@ previousItem.addEventListener('click', showPreviousItem);
 document.addEventListener('keydown', keyPress);
 </script>
 
+<?=template_menu()?>
 <?=template_footer()?>
 </body>
 </html>
